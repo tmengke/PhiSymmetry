c15297ee5b2d0ffddd2a743cbcd3002f588b98bf
nilina <Natalia.Lychkovskaya@cern.ch>
2017-11-29 08:52:48 +0100

    Add files via upload

---
+++ Iterative/skipHEChannel.C
@@ -0,0 +1,8 @@
+bool skipHEChannel(int iphi,int ieta) {
+  if (
+      //      (iphi>=55 && iphi<=56 && ieta<=-16)
+      //(iphi>=55 && iphi<=58 && ieta<=-16) // also something...
+      )
+    return true;
+  else return false;
+}
